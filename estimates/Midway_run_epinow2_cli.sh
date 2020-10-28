210990facdd261462d40469c438c1bf39c4752ed
kgostic <kategostic@gmail.com>
2020-10-28 16:16:23 -0400

    more midway debugging

--- estimates/Midway_run_epinow2_cli.sh
+++ estimates/Midway_run_epinow2_cli.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=03:00:00
+#SBATCH --time=03:30:00
 #SBATCH --output=/project2/cobey/covid-modeling/rt-pipeline-09-2020/estimates/midway/%A.out
 #SBATCH	--error=/project2/cobey/covid-modeling/rt-pipeline-09-2020/estimates/midway/%A.err
 #SBATCH --nodes=12
-#SBATCH --array=2-13
+#SBATCH --array=1-12
 #SBATCH --ntasks-per-node=4
 #SBATCH --mem-per-cpu=4000
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
