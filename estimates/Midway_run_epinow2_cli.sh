ced4fb91808988e7889fbea5e6cb29809c9a3e99
kgostic <kategostic@gmail.com>
2020-10-30 13:28:34 -0400

    Updated bash script

--- estimates/Midway_run_epinow2_cli.sh
+++ estimates/Midway_run_epinow2_cli.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #SBATCH --time=04:30:00
-#SBATCH --output=/project2/cobey/covid-modeling/rt-pipeline-09-2020/estimates/midway/%A.out
-#SBATCH	--error=/project2/cobey/covid-modeling/rt-pipeline-09-2020/estimates/midway/%A.err
-#SBATCH --nodes=12
+#SBATCH --output=midway/%A_%a.out
+#SBATCH	--error=midway/%A_%a.err
 #SBATCH --array=1-12
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-cpu=4
 #SBATCH --mem-per-cpu=1000
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=kgostic
-#SBATCH --qos=covid-19
-#SBATCH --account=covid-19 -p broadwl
+#SBATCH --partition=cobey
+
+
+## #SBATCH --qos=covid-19
+## #SBATCH --account=covid-19 -p broadwl
 
 
 ## Set working directory
@@ -23,4 +25,4 @@ cp /project2/cobey/covid-modeling/rt-pipeline-09-2020/data/cli_admissions_2929-1
 
 ## Run the Rt estimation pipeline
 
-Rscript estimate_cli_epinow2.R --var=$SLURM_ARRAY_TASK_ID --midway=TRUE
+Rscript estimate_cli_epinow2.R --var=$SLURM_ARRAY_TASK_ID --midway=TRUE --debug=TRUE
