249e0a84e951e79998b56160dc759c9dff5a1fa5
kgostic <kategostic@gmail.com>
2020-11-03 11:00:59 -0800

    Fixed the output filepath when debugging

--- code/summarise-epinow2-estimates.R
+++ code/summarise-epinow2-estimates.R
@@ -3,38 +3,34 @@ summarise_all_estimates <- function(
   path, 
   dt, #Late date in time series
   tooday, # Date on which analysis is run
-  outpath = NULL # Optional path for outputs
+  debug
 ){
+
   
-  library(optparse)
-  ## Read in options from midway
-  option_list = list(make_option("--debug", type = "logical", default = FALSE, help = 'optional outpath spec for testing. Default is epinow2_cli_estimates/')); 
-  opt_parser = OptionParser(option_list=option_list);
-  opt = parse_args(opt_parser); # Now you have a list called "opt" with elements opt$var and opt$out
-  
-  
-  dirs <- list.files(path)
-  dirs <- dirs[!grepl(dirs, pattern = '(\\d\\d\\d\\d-\\d\\d-\\d\\d)')] # Drop the directories named for dates, not regions
-  
-  if(opt$debug==TRUE){
+  if(debug){
     full_outpath =  sprintf('%s/full_outputs.csv', path)
     rt_outpath =    sprintf('%s/Rt_outputs.csv', path)
   }else{
     full_outpath =  sprintf('../figs/cli_nadmit_%s/full_outupts_epinow2_%s.csv', tooday, dt)
     rt_outpath =    sprintf('../figs/cli_nadmit_%s/Rt_outupts_epinow2_%s.csv', tooday, dt)
   }
+  
+  
+  dirs <- list.files(path)
+  dirs <- dirs[!grepl(dirs, pattern = '(\\d\\d\\d\\d-\\d\\d-\\d\\d)')] # Drop the directories named for dates, not regions
+  
   #dirs <- dirs[!dirs %in% regions_to_exlude]
   ## Write a function to read in outputs from one region
   read_one_estimate <- function(rr, path, dt){
     if(file.exists(sprintf('%s/%s/%s/summarised_estimates.rds', path, rr, dt))){
-      ss <- read_rds(sprintf('%s/%s/%s/summarised_estimates.rds', path, rr, dt))
+      ss <- readr::read_rds(sprintf('%s/%s/%s/summarised_estimates.rds', path, rr, dt))
     }else{
       ss <- NULL
     }
     return(ss)
   }
   fulldf <- lapply(dirs, function(rr) read_one_estimate(rr, path, dt)) %>%
-    set_names(dirs) %>%
+    rlang::set_names(dirs) %>%
     bind_rows(.id = 'region')
   Rtdf <- filter(fulldf, variable=='R')
   write_csv(fulldf, path = full_outpath)
